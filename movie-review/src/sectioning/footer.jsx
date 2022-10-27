2eed08144517027fe68c154db8011e33bc2e3b79
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-10-26 23:45:47 -0700

    add footer changes

--- movie-review/src/sectioning/footer.jsx
+++ movie-review/src/sectioning/footer.jsx
@@ -9,11 +9,13 @@ const Footer = () => {
             <NavLink to="./contact">Contact</NavLink>
             <NavLink to="./about">About</NavLink>
             <NavLink to="/"><ion-icon name="library-outline"></ion-icon></NavLink>
-            <p>Follow:</p>
-            <NavLink><ion-icon name="logo-twitter"></ion-icon></NavLink>
-            <NavLink><ion-icon name="logo-facebook"></ion-icon></NavLink>
-            <NavLink><ion-icon name="logo-instagram"></ion-icon></NavLink>
-            <NavLink><ion-icon name="logo-github"></ion-icon></NavLink>
+            <div class="follow-buttons">
+              <p>Follow:</p>
+              <NavLink><ion-icon name="logo-twitter"></ion-icon></NavLink>
+              <NavLink><ion-icon name="logo-facebook"></ion-icon></NavLink>
+              <NavLink><ion-icon name="logo-instagram"></ion-icon></NavLink>
+              <NavLink><ion-icon name="logo-github"></ion-icon></NavLink>
+            </div>  
         </nav>
       </div>
     </footer>
