578f9060ef1f47b297a69e90439c747525d2d072
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-11-16 19:34:15 -0800

    add styling changes

--- movie-review/react-ui/src/index.js
+++ movie-review/react-ui/src/index.js
@@ -5,18 +5,28 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './app/store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import { fetchPopularMovies } from './components/popular-movies/slice';
+import { fetchPopularTvShows } from './components/popular-tv-shows/slice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
 
+async function init(){
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  store.dispatch(fetchPopularMovies())
+  store.dispatch(fetchPopularTvShows())
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+init()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
