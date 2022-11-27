700e72f6eac34e4c29e85c6df05b4245a6579107
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-11-27 13:52:22 -0800

    add header nav changes

--- movie-review/react-ui/src/sectioning/header.jsx
+++ movie-review/react-ui/src/sectioning/header.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import NavSearch from '../components/search';
 import { NavLink } from 'react-router-dom';
 import { useRef } from 'react';
+import { useState } from 'react';
 
 const Header = () => {
   const navRef = useRef();
+  const [showNav, setShowNav] = useState(true)
 
-  const showNavBar = () => {
-      navRef.current.classList.toggle("responsive_nav")
-      alert('hello')
+  const handleClick = () => {
+    alert("hello")
+    setShowNav(!showNav)
   }
 
   return (
@@ -16,13 +18,13 @@ const Header = () => {
       <div className="y-wrap">
         <nav className="navbar">
             <NavLink to="/" className="nav-title"><ion-icon name="library-outline"></ion-icon>The Backlog Center</NavLink>
-            <NavLink to="#" onClick={showNavBar} className="toggle-button">
+            <NavLink to="#" ref={navRef} onClick={handleClick} className="toggle-button">
               <span className="bar"></span>
               <span className="bar"></span>
               <span className="bar"></span>
             </NavLink>
-            <div ref={navRef} className="navbar-links" >
-              <ul className="navbar-links-container">
+            <div className="navbar-links" >
+              <ul ref={navRef} onClick={handleClick} className="navbar-links-container">
                 <li><NavLink to="./movies">Movies</NavLink></li>
                 <li><NavLink to="./shows">Shows</NavLink></li>
                 <li><NavSearch /></li>
