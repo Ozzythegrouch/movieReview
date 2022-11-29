eab5c4d79ddc7510109b5ad845f618d74a5646ac
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-11-29 15:02:39 -0800

    add store updates

--- movie-review/react-ui/src/app/store.js
+++ movie-review/react-ui/src/app/store.js
@@ -3,13 +3,20 @@ import popularMoviesReducer from '../components/popular-movies/slice'
 import popularTvShowsReducer from '../components/popular-tv-shows/slice'
 import addMovieReducer from '../components/popular-movies/addMovieSlice';
 import addMovieToDoneReducer from '../components/popular-movies/addMovieDoneSlice';
-
+import addTvShowReducer from '../components/popular-tv-shows/addTvShowSlice'
+import upcomingMoviesReducer from '../components/upcoming-movies/slice';
+import dailyPopularReducer from '../components/daily-popular/slice';
+import topRatedTvShowsReducer from '../components/daily-popular-show/slice';
 
 export default configureStore({
     reducer: {
         popularMovies: popularMoviesReducer,
         popularTvShows: popularTvShowsReducer,
         addMovieToWatch: addMovieReducer,
-        addMovieToDone: addMovieToDoneReducer
+        addMovieToDone: addMovieToDoneReducer,
+        addTvShowToWatch: addTvShowReducer,
+        upcomingMovies: upcomingMoviesReducer,
+        dailyPopular: dailyPopularReducer,
+        topRatedTvShows : topRatedTvShowsReducer
     }
 })
